ef90743a7a0ddc5ecf8276590a4dfe9f11830704
adiutant <mikolaj@marasek.eu>
2023-12-06 12:21:25 +0100

    some bugs to fix related to fetch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,22 +8,54 @@ const handleButtonClicked = (inputValue) => {
 }
 function App(props) {
     const KEY = 'f67972513bfe74144ead3e32efb5e00a'
-    const LONGITUDE = '10.99'
-    const LATITUDE = '44.34'
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${LATITUDE}&lon=${LONGITUDE}&appid=${KEY}`
+    const CITYNAME = 'Kraków'
+    const getCityCoordsLink = `http://api.openweathermap.org/geo/1.0/direct?q=${CITYNAME}&limit=${'1'}&appid=${KEY}`
+    console.log(getCityCoordsLink)
+
+
+
     const [data,setData] = useState(null)
     const [inputValue,setInputValue] = useState('')
 
     useEffect(() => {
-        fetch(apiUrl)
-            .then(response => response.json())
-            .then((data) => {
-                setData(data)
-            })
-            .catch((error) => {
-                console.error(error)
-            })
+        let getWeatherLink;
+        //get a coordinates
+            fetch(getCityCoordsLink)
+                .then(response => response.json())
+                .then((data) => {
+                    setData(data)
+                    const {lat, lon} = data[0]
+                    getWeatherLink = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${KEY}`
+                    console.info(lat,lon)
+                })
+                .catch((error) => {
+                    console.error(error)
+                })
+
+
+        setData(null)
+
+
+
+                console.info('1st fetch done')
+                console.info(getWeatherLink)
+        //get a weather
+            fetch(getWeatherLink)
+                .then(response => response.json())
+                .then((data) => {
+                    setData(data)
+                })
+                .catch((error) => {
+                    console.error(error)
+                })
+        console.log(data)
+
+
     },[])
+
+
+
+
     console.log(data)
 
 
