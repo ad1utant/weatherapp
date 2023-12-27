a924adf21cf8449587cfaf1b5af50710e924b04a
adiutant <mikolaj@marasek.eu>
2023-12-27 21:20:05 +0100

    add some proporties

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -41,24 +41,48 @@ function App(props) {
         }
     },[cityName])
 
-    const { main, weather, wind, sys, name, coord } = data || {};
+    const { visibility, main, weather, wind, sys, name, coord } = data || {};
     const {deg, speed} = wind || {};
+    const {description, main: cloudsMain} = (weather && weather.length > 0) ? weather[0] : {};
     let { feels_like, humidity, pressure, temp, temp_max, temp_min } = main || {};
     temp = Math.round(temp - 273)
     temp_max = Math.round(temp_max - 273)
     temp_min = Math.round(temp_min - 273)
     feels_like = Math.round(feels_like - 273)
+
     return (
         <div className="App">
             <Form inputValue = {inputValue} setInputValue = {setInputValue} handleButtonClicked = {handleButtonClicked} />
-            <h1>Temperature</h1>
-            <p>max temperature: {temp_max}</p>
-            <p>temperature: {temp}</p>
-            <p>min temperature: {temp_min} </p>
-            <p>feels like: {feels_like}</p>
-            <h1>Wind</h1>
-            <p>speed: {speed}</p>
-            <p>deg: {deg}</p>
+
+            <div className={'temperatureBox'}>
+                <h1>Temperature</h1>
+                <p>max temperature: {temp_max}</p>
+                <p>temperature: {temp}</p>
+                <p>min temperature: {temp_min} </p>
+                <p>feels like: {feels_like}</p>
+            </div>
+
+
+            <div className={'windBox'}>
+                <h1>Wind</h1>
+                <p>speed: {speed}</p>
+                <p>deg: {deg}</p>
+            </div>
+
+            <div className={'otherBox'}>
+                <h1>Other</h1>
+                <p>humidity: {humidity}</p>
+                <p>pressure: {pressure}</p>
+
+            </div>
+
+            <div>
+                <h1>Clouds</h1>
+                <p>visibility: {visibility}</p>
+                <p>clouds: {cloudsMain}</p>
+                <p>description: {description}</p>
+
+            </div>
 
         </div>
     )
